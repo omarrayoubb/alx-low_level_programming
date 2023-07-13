632e85015f5381fa76358cd14f9a47f3888189bf
omar yasser <omaryasser1865@gmail.com>
2023-07-13 09:10:31 -0700

    6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - it simply print a word
+ *
+ * Description: programming meaning in a big way
+ *
+ * Return: 0 means success
+ */
+
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
