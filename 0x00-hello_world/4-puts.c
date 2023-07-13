fb19594bea2aea5741ee919e09ae4fa9aac32764
omar yasser <omaryasser1865@gmail.com>
2023-07-13 09:02:42 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-
+/**
+ * main - it simply print a word 
+ *
+ *
+ * Description : programming meaning in a big way
+ *
+ *Return: 0 means success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
