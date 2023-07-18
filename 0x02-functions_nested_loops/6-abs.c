f18e7284f17aeb7c2b39814f4d21c846b5397016
omar yasser <omaryasser1865@gmail.com>
2023-07-18 06:08:33 -0700

    mm

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - show lowercase upper
+ * @r: integer
+ * Description: show me
+ *
+ * Return: 0 means no 1 means yes
+ */
+int _abs(int r)
+{
+	if (r < 0)
+		r = r * -1;
+	return (r);
+}
