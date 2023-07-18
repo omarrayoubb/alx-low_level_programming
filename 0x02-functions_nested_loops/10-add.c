a159118d1a79a323e97b14618e5bd33898e2e83d
omar yasser <omaryasser1865@gmail.com>
2023-07-18 06:51:43 -0700

    hello

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add 2 numbers
+ * @a: first number
+ * @b: second number
+ * Description: add 2 numbers together m3an
+ *
+ * Return: sum of 2 num
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
