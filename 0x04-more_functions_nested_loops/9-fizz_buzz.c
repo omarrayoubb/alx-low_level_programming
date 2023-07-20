d23007a9117c83cd8ae210ef071923112be4e8e2
omar yasser <omaryasser1865@gmail.com>
2023-07-20 05:04:04 -0700

    0x09

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - fizz buzz
+ *
+ * Description: fiz buzz printa
+ *
+ * Return: 0 alwasys means success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("%s ", "FizzBuzz");
+		else if (i % 5 == 0)
+			printf("%s ", "Buzz");
+		else if (i % 3 == 0)
+			printf("%s ", "Fizz");
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
