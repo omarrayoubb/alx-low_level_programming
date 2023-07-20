03a3251925794b8c6cd585e8d0fd1a06d27ca322
omar yasser <omaryasser1865@gmail.com>
2023-07-20 02:15:58 -0700

    0x04

---
+++ 0x04-more_functions_nested_loops/3-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - print
+ *
+ * Description: print numbers from 0-9
+ *
+ *
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i / 1 + '0');
+		i++;
+	}
+	_putchar('\n');
+}
