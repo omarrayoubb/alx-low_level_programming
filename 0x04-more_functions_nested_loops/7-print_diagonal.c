8964738644df61eb65370af45475a97a85f286b4
omar yasser <omaryasser1865@gmail.com>
2023-07-20 03:38:28 -0700

    0x04

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - print
+ * @n: input
+ * Description: print a diagonal
+ *
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	if (n == 0)
+		_putchar('\n');
+	while (i < n)
+	{
+		j = 0;
+		while (j <= i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+}
