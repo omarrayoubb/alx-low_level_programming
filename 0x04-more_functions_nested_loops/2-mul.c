a2abf26d8e194b28ca40f7d099a622eb38a62b0c
omar yasser <omaryasser1865@gmail.com>
2023-07-20 02:03:53 -0700

    hello world

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiple
+ * @a: first number
+ * @b: second number
+ * Description: multiple 2 numbers
+ *
+ * Return: multiple the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
