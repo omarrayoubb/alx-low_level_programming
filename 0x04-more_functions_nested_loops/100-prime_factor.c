885faad9c5eb83700f30b119b03650afb7a3ac7a
omar yasser <omaryasser1865@gmail.com>
2023-07-21 04:14:03 -0700

    4019

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,18 +10,19 @@
 
 int main(void)
 {
-	long int i, n = 612852475143, l;
+	int i;
+	long n = 612852475143;
 
-	for (i = 2 ; i <= sqrt(n); i++)
+	for (i = (int) sqrt(n); i > 2; i++)
 	{
 		if (n % i == 0)
 		{
-			l = i;
-			while (n % i == 0)
-				n /= i;
+			printf("%d\n", i);
+			break;
 		}
 	}
-	printf("%ld \n", l);
+
 	return (0);
 }
+	
 
