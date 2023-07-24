a9796236e4cd6af1ea003697e9ad5f3468c8b00f
omar yasser <omaryasser1865@gmail.com>
2023-07-24 06:42:27 -0700

    0x08

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,7 +13,9 @@ void print_array(int *a, int n)
 	int i;
 
 	if (n <= 0)
-		n *= -1;
+		printf("\n");
+	else
+	{
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
@@ -21,6 +23,7 @@ void print_array(int *a, int n)
 			printf("\n");
 		else
 			printf(", ");
+	}
 	}
 }
 
