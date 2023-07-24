7257988f6f2b08ed15e3907698e575e47e9c7c99
omar yasser <omaryasser1865@gmail.com>
2023-07-24 06:15:35 -0700

    0x06

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ *puts_half - print
+ *@str: input
+ *Description: print to stdout
+ *
+ *
+ */
+void puts_half(char *str)
+{
+	unsigned long int i, n;
+
+	if (strlen(str) % 2 == 0)
+	{
+		n = strlen(str) / 2;
+	}
+	else
+	{
+		n = ((strlen(str) - 1) / 2);
+	}
+	for (i = n; i < strlen(str); i++)
+	{
+		_putchar(str[i]);
+
+	}
+	_putchar('\n');
+}
