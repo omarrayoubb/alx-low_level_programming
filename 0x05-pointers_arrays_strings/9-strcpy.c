6fc29845c4a35721102504be4d4cad27bcb1f347
omar yasser <omaryasser1865@gmail.com>
2023-07-24 06:53:30 -0700

    0x09

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+/**
+ **_strcpy - copy
+ * @dest:input
+ * @src:input
+ *Description: copy array
+ *
+ *Return: dest char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
+
