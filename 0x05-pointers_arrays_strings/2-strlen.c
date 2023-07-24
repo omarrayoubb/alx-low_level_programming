45615b972a0d448c262af2acb2aea4571699676c
omar yasser <omaryasser1865@gmail.com>
2023-07-24 03:31:44 -0700

    0x02

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - string len
+ * @s: char
+ * Description: char and string length
+ *
+ * Return: n the length of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
