1ad15eaced2c2b927db981af567e8b293154ffa9
omar yasser <omaryasser1865@gmail.com>
2023-07-31 07:07:49 -0700

    0x02

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+define NULL ((void *)0)
 /**
  * _strchr - char
  * @s: input
@@ -19,5 +20,5 @@ char *_strchr(char *s, char c)
 		}
 		i++;
 	}
-	return ('\0');
+	return (NULL);
 }
