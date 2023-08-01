5ca73de319695880bf7b346ac8ca618bc966cc38
omar yasser <omaryasser1865@gmail.com>
2023-08-01 04:53:10 -0700

    0x07

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,16 +12,14 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		if (a[i][0] != ' ')
-		{
 		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
-		}
-			_putchar('\n');
+		
+			
 
 		}
-		else
-			_putchar('\n');
+		_putchar('\n');
+		
 	}
 }
