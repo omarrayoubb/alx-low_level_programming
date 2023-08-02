83cb3958261ea94a37a0a79543765d8fb591a38b
omar yasser <omaryasser1865@gmail.com>
2023-08-02 08:15:20 -0700

    0x05

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - print
+ * @n: input
+ * Description: hello
+ *
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0) 
+		return -1;
+	if (n == 0)
+		return 1;
+	return n * factorial(n - 1);
+}
