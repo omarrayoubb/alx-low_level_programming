744fbc3aa840daec55f9a4ea5123a9c1a7bbccd0
omar yasser <omaryasser1865@gmail.com>
2023-08-09 10:43:35 -0700

    0x02

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include "stdio.h"
+#include "stdlib.h"
+/**
+ * main - input
+ * @argc: counter
+ * @argv: strings
+ * Description: say hello to my little friend
+ *
+ * Return: 0 means success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, count = 0, x = atoi(argv[argc - 1]);
+		int arr[5] = {25, 10, 5, 2, 1};
+
+		for (i = 0 ; i < 5; i++)
+		{
+			if (x >= arr[i])
+			{
+				count += x / arr[i];
+				x = x % arr[i];
+				if (x % arr[i] == 0)
+					break;
+			}
+		}
+		printf("%d\n", count);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
