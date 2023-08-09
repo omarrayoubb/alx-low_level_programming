5e38d5bb5eab7c67e3cf1d8d22c073869e096f77
omar yasser <omaryasser1865@gmail.com>
2023-08-09 09:01:26 -0700

    0x03

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+#include "stdlib.h"
+/**
+ * main - input
+ * @argc: counter
+ * @argv: strings
+ * Description: say hello to my little friend
+ *
+ * Return: 0 means success
+ */
+int main(int argc, char *argv[])
+{
+	int i, x, result = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			x = atoi(argv[i]);
+			if (x == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				result += x;
+		}
+	printf("%d\n", result);
+	}
+	return (0);
+}
