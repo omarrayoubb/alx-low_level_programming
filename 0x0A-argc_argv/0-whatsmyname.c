d9612dbde265d1926ede2e503c2f8fbd98057d3a
omar yasser <omaryasser1865@gmail.com>
2023-08-09 07:01:01 -0700

    0x02

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+/**
+ * main - input
+ * @argc: counter
+ * @argv: strings
+ * Description: say hello to my little friend
+ *
+ * Return: 0 means success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
