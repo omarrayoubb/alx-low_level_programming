ee5b114a5bfa0ceb2d7ec767ae708c1d460a83ef
omar yasser <omaryasser1865@gmail.com>
2023-08-13 09:24:58 -0700

    0x002

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * alloc_grid - alloccate
+ * @grid: width
+ * @height: height
+ * Description: hello worled
+ *
+ *
+ */
+void free_grid(int **grid, int height)
+{
+
+	int i;
+
+	for (i = 0 ; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
