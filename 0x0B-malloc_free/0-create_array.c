1622063fe52eb097cfef48fdd98f1fe341f4e28c
omar yasser <omaryasser1865@gmail.com>
2023-08-12 17:11:21 -0700

    0x0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *buffer = (char *)malloc(sizeof(char) * size);
 
+	if (size == 0)
+		return ('\0');
 	while (i < size)
 	{
 		buffer[i] = c;
