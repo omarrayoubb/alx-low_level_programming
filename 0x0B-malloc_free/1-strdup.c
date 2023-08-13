3fa24a7ef30b06771a4da7b53f1a75937521d009
omar yasser <omaryasser1865@gmail.com>
2023-08-13 06:00:34 -0700

    0x00

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,11 +17,12 @@ char *_strdup(char *str)
 	{
 		return ('\0');
 	}
-	std = (char *) malloc(strlen(str));
+	std = (char *) malloc(strlen(str) + 1);
 	while (str[i] != '\0')
 	{
 		*(std + i) = *(str + i);
 		i++;
 	}
 	return (std);
+	free(std);
 }
