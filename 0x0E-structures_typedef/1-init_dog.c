978b2b6d8deef4a2520882a149ce12b391a17543
omar yasser <omaryasser1865@gmail.com>
2023-08-15 23:29:45 +0000

    0x00

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+*init_dog - init
+*@d: dog
+*@name: name of the dog
+*@age: age of him
+*@owner: the owner of the dog
+*Description: information about the dog
+*
+*
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
