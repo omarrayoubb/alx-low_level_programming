35fd8d8139cbe525fccd36903a70967cb94d443b
omar yasser <omaryasser1865@gmail.com>
2023-08-16 00:59:30 +0000

    x0x0

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,14 +19,7 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: %s\n", d->name);
 		}
-		if (d->age == 0.0)
-		{
-			printf("Age: (nil)\n");
-		}
-		else
-		{
-			printf("Age: %f\n", d->age);
-		}
+		printf("Age: %f\n", d->age);
 		printf("Owner: ");
 
 		if (d->owner == NULL)
