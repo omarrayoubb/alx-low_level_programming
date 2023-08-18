e63ee5377020e49a735fd47c9a5bbbc9c4bfbc18
omar yasser <omaryasser1865@gmail.com>
2023-08-18 01:58:02 +0000

    0x00

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,7 @@
 #ifndef _variadic_functions_h
 #define _variadic_functions_h
+#include <stdarg.h>
+#include <stdiolib.h>
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
