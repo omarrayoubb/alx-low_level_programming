b083b34857368432f110736a6059f52acdbec19b
omar yasser <omaryasser1865@gmail.com>
2023-08-19 21:13:06 +0000

    0x00

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+* array_iterator - print
+* @array: input
+* @size: input
+* @action: function
+*Description: print the indexe
+*
+*
+*/
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+	unsigned int i = 0;
+
+	for (; i < size; i++)
+		action(array[i]);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+/**
+* int_index - index return
+* @array: array
+* @size: size of the array
+* @cmp: comparison function
+* Description: return the index of the first appearance
+*
+* Return: index or -1
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	int index = 0;
+
+	if (size <= 0)
+		return (-1);
+	for (; i < size; i++)
+	{
+		index = cmp(array[i]);
+		if (index > 0)
+			break;
+	}
+	if (index == 0)
+		return (-1);
+	return (i);
+}
