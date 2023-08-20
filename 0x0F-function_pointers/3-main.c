5f03e6817b452c619eb040888c1261c39e196a48
omar yasser <omaryasser1865@gmail.com>
2023-08-20 01:35:52 +0000

    0x00

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (!argv[2] || strlen(argv[2] > 1))
+	if (!argv[2] || strlen(argv[2]) > 1)
 	{
 		printf("Error\n");
 		exit(99);
