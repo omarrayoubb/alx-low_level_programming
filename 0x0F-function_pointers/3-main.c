8fb7cdfca7c928acb2582c540a593e8b83da4bc3
omar yasser <omaryasser1865@gmail.com>
2023-08-20 01:11:58 +0000

    0x00

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include "stdio.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+* main - ...
+* @argc: ...
+* @argv: ...
+* Description: .......
+*
+* Return: 0 if success or 98 99 100 if error or null
+*/
+int main(int argc, char *argv[])
+{
+	int a = atoi(argv[1]), b = atoi(argv[3]);
+	int (*sum)(int, int);
+
+	sum = get_op_func(argv[2]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (!argv[2] || strlen(argv[2]) > 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((atoi(argv[3]) == 0) && (*argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", sum(a, b));
+	return (0);
+}
