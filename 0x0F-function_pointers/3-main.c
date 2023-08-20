c1465308aee4a085dd36ace44369511842ba4063
omar yasser <omaryasser1865@gmail.com>
2023-08-20 01:24:31 +0000

    0x00

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,11 +26,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if ((atoi(argv[3]) == 0) && (*argv[2] == '/' || *argv[2] == '%'))
+/*	if ((atoi(argv[3]) == 0) && (*argv[2] == '/' || *argv[2] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
-	}
+	}*/
 	printf("%d\n", sum(a, b));
 	return (0);
 }
