d1ef9f98f76dba3be556566c042fb5a2ae895806
omar yasser <omaryasser1865@gmail.com>
2023-08-25 01:14:50 +0000

    Print

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+/**
+* print_list - print
+* @h: list
+* Description: print a list
+*
+* Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+			printf("[%d] %s\n", strlen(h->str), h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
