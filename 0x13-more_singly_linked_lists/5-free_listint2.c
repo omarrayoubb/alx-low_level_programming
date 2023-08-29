cfec7d0738a75d5d9894b923d8a2fd25f1270d39
omar yasser <omaryasser1865@gmail.com>
2023-08-29 19:17:21 +0000

    920

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* free_listint2 - free
+* @head: head
+* Description: hello worlld
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *x = *head;
+
+	while (*head)
+	{
+		x = (*head)->next;
+		free(*head);
+		*head = NULL;
+		*head = x;
+	}
+}
