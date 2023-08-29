e6cc19b9a62e30723e8dcd7934d92dea543755ec
omar yasser <omaryasser1865@gmail.com>
2023-08-29 18:40:39 +0000

    0x02

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* add_nodeint - add
+* @head: pointer
+* @n: variable
+* Description: hello dsflksdf
+* Return: new list
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *tmp = malloc(sizeof(listint_t));
+	listint_t *ptr = *head;
+
+	if (!n || !head)
+		return (NULL);
+	tmp->n = n;
+	tmp->next = ptr;
+	*head = tmp;
+	return (tmp);
+}
