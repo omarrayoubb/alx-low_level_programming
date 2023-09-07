a8a112f2408f3c889daf3e26ff7884297fdd1aac
omar yasser <omaryasser1865@gmail.com>
2023-09-07 21:48:29 +0000

    0x00

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <math.h>
+#include "string.h"
+#include "stdlib.h"
+#include "stdio.h"
+/**
+* binary_to_uint - binary
+* @b: input
+* Description: binary to uint
+* Return: i the number
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	int n, sum = 0, base = 1, x;
+
+
+	if (!b)
+		return (0);
+	n = atoi(b);
+	while (*b)
+	{
+		if (*b < '0' || *b > '1')
+			return (0);
+		b++;
+		continue;
+	}
+	if (!n)
+		return (0);
+	while (n > 0)
+	{
+		x = n % 10;
+		if (x < 0 || x > 1)
+			return (0);
+		sum = sum + x * base;
+		base = base * 2;
+		n = n / 10;
+	}
+	return (sum);
+}
