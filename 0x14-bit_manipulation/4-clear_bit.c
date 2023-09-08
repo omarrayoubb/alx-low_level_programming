b36d9440502029edf1c1915da32dea924620bf60
omar yasser <omaryasser1865@gmail.com>
2023-09-08 15:09:57 +0000

    0x000

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* clear_bit - input
+* @n: input
+* @index: input
+* Description: hello world
+* Return: index or -1 if error occured
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int current;
+
+	if (index == 0)
+	{
+		*n = *n & 0;
+		return (1);
+	}
+	else if (index > 63)
+		return (-1);
+	current = 1 << index;
+	*n = *n & (~current);
+	return (1);
+
+
+}
